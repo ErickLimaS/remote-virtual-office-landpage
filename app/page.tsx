aeaa65853bee966c8985739ea3352c1810674fc8
Erick Lima <ericklima63@gmail.com>
2024-09-07 21:10:48 -0300

    feat: Add Metadata to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import HeroSection from "./components/HomePage/HeroSection";
 import PageFooter from "./components/HomePage/PageFooter";
 import VideoSupportOutroSection from "./components/HomePage/VideoSupportOutroSection";
 
+export async function generateMetadata() {
+  return {
+    title: "RoboMeet - Virtual office for remote teams",
+    description: `RoboMeet offers a dynamic virtual workspace for remote teams, providing an immersive environment with tools for communication, collaboration, and project management.`,
+  };
+}
+
 export default function Home() {
   return (
     <main className="container mt-20">
