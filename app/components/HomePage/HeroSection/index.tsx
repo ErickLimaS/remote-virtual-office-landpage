b5725f9f547c444407a045c8b4d5c1e4f1cd6eca
Erick Lima <ericklima63@gmail.com>
2024-09-08 12:58:54 -0300

    refactor: Increase Home Hero Section img quality

--- app/components/HomePage/HeroSection/index.tsx
+++ app/components/HomePage/HeroSection/index.tsx
@@ -4,7 +4,7 @@ import SignUpButton from "../../buttons/SignUpButton";
 
 function HeroSection() {
   return (
-    <section className="grid md:grid-cols-2 gap-4 lg:gap-24">
+    <section className="grid md:grid-cols-[1fr,60%] gap-4 lg:gap-24">
       <div className="flex flex-col my-auto gap-y-12">
         <h1 className="text-5xl sm:text-6xl font-black">
           {`Wherever you are, you're better together.`}
@@ -14,14 +14,14 @@ function HeroSection() {
           Virtual office for remote teams
         </p>
 
-        <div>
+        <div className="max-sm:flex max-sm:flex-col">
           <SignUpButton />
         </div>
       </div>
 
-      <div className="relative max-h-[576px] w-full aspect-[5/5]">
+      <div className="relative max-h-[576px] w-full aspect-[12/9]">
         <Image
-          src={"/imgs/homepage/hero.png"}
+          src={"/imgs/homepage/hero.jpeg"}
           alt="People on a Virtual Office"
           fill
           quality={100}
